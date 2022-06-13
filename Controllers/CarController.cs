4a6b3ef94600560d3b9f997d116e34e4ea2cc519
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-06-13 23:55:28 +0500

    controler

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using TaxiDrivers.Entities;
+using TaxiDrivers.Models;
 using TaxiDrivers.Services;
 
 namespace TaxiDrivers.Controllers;
+[ApiController]
+[Route("api/[controller]")]
 
  public class CarController : ControllerBase
  {
@@ -12,6 +16,22 @@ namespace TaxiDrivers.Controllers;
     {
         _logger = logger;
         _service = service;
+    }
+    [HttpPost("/addcar")]
+    public async Task<IActionResult> AddCar([FromForm]NewCar newCar)
+    {
+        var car = new Car()
+        {
+            id = Guid.NewGuid(),
+            Model = newCar.Model,
+            Color = newCar.Color,
+            Number = newCar.Number,
+            Type = newCar.Type
+        };
+        var result = await _service.InsertAsync(car);
+        var error = !result.ISucces;
+        var message = result.e.Message;
+        return Ok(new { error, message });
     }
 
 }
